df667e13fe8bde52d5873aee9f6c7488a4373af3
k34r <k34rrr@gmail.com>
2023-07-10 20:49:06 +0700

    добавил модальное окно из materialize

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.sidenav');
-    let instances = M.Sidenav.init(elems);
+    let elems_sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = M.Sidenav.init(elems_sidenav);
+
+    let elems_modal = document.querySelectorAll('.modal');
+    let modal = M.Modal.init(elems_modal);
 });
